e4172c3777eb5dabbf0dbeec45d817621f96d7a9
polinakoroleva <polinakorolevaaaaa@gmail.com>
2023-12-22 14:09:00 +0700

    update App.jsx

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,11 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
 import TaskForm from './Form';
 import TaskList from './TasksList';
+import { actions as tasksActions, selectors } from '../slices/tasksSlice';
 
-const App = () => (
-  <>
-    <TaskForm />
-    <TaskList />
-  </>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  const tasks = useSelector(selectors.selectAll);
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    if (storedTasks) {
+      dispatch(tasksActions.setTasks(storedTasks));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  return (
+    <>
+      <TaskForm />
+      <TaskList />
+    </>
+  );
+};
 
 export default App;
